d4c7f5c68dc773680eeadcb70f3134b34148d33d
Aybike <100150878+aybikedalbul@users.noreply.github.com>
2023-03-16 17:51:19 +0300

    Created Slugify

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const pageRoute = require('./routes/pageRoutes')
+const courseRoute = require('./routes/courseRoute')
+
 
 const app = express();
 
+// Connect DB 
+mongoose.connect('mongodb://127.0.0.1/smartedu-db')
+.then(() =>{ 
+  console.log('DB Connected Succesful')
+})
 //Templete Engine
-app.set('view engine', "ejs");
+app.set('view engine', 'ejs');
 
 //Middleware
-app.use(express.static("public"))
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.status(200).render('index', {
-    page_name: "index"
-  })
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use('/', pageRoute)
+app.use('/courses', courseRoute)
+
+app.get('/dashboard', (req, res) => {
+  res.status(200).render('dashboard', {
+    page_name: 'dashboard',
+  });
 });
-app.get('/about', (req, res) => {
-  res.status(200).render('about', {
-    page_name: "about"
-  })
+app.get('/contact', (req, res) => {
+  res.status(200).render('contact', {
+    page_name: 'contact',
+  });
 });
-
-
 
 const port = 3003;
 
