af8de6df881908c579789dc5e4c3aa5a86439308
Aybike <100150878+aybikedalbul@users.noreply.github.com>
2023-03-16 22:01:45 +0300

    test

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoutes')
 const courseRoute = require('./routes/courseRoute')
+const categoryRoute = require('./routes/categoryRoute')
 
 
 const app = express();
@@ -21,6 +22,7 @@ app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use('/', pageRoute)
 app.use('/courses', courseRoute)
+app.use('/categories', categoryRoute)
 
 app.get('/dashboard', (req, res) => {
   res.status(200).render('dashboard', {
