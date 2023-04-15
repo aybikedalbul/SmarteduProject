679591e653f6f39bc50a0394b898072f05a4ccfc
Aybike <100150878+aybikedalbul@users.noreply.github.com>
2023-04-15 13:51:03 +0300

    Send e-mail

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,5 @@
+const nodemailer = require("nodemailer");
+
 exports.getIndexPage = (req, res) => {
   console.log(req.session.userID);
   res.status(200).render('index', {
@@ -23,3 +25,41 @@ exports.getLoginPage = (req, res) => {
   });
 };
 
+exports.getContactPage = (req, res) => {
+  res.status(200).render('contact', {
+    page_name: 'contact',
+  });
+};
+exports.sendEmail = async (req, res) => {
+  const outputMessage = `
+  <h1>Mail Details </h1>
+  <ul>
+  <li>Name: ${req.body.name} </li> 
+  <li>Email: ${req.body.email} </li>
+  </ul>
+  <h1>Message </h1>
+  <p>${req.body.message}</p>
+  `
+  // Created using Ethereal.mail: 
+   let transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    secure: false,
+    auth: {
+      user: "geovanny18@ethereal.email",
+      pass: "mdQAcrXcfkjpwK5ex1"
+    }
+  });
+
+  let info = await transporter.sendMail({
+    from: '"Smart EDU Contact  Form" , <geovanny18@ethereal.email>',
+    to: 'aybikedalbul@hotmail.com',
+    subject: 'Smart EDU contact from new message',
+    html: outputMessage,
+  });
+
+  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+  res.status(200).redirect('contact')
+ 
+}
